d63e1aa0dfbe402b319714028e890d682ecdb9e4
adonaiviedondon <vieiraadonai@gmail.com>
2025-12-30 11:37:13 -0300

    Update AbelhasOperarias.py

--- src/bees/AbelhasOperarias.py
+++ src/bees/AbelhasOperarias.py
@@ -2,14 +2,24 @@ import numpy as np
 import random
 
 class AbelhasOperarias:
-    def __init__(self,eficiencia_fn,opcaoSolucao,contadorInteacoesSemMelhoria,valorEficiencia):
-        self.eficiencia_fn = eficiencia_fn
-        self.opcaoSolucao = opcaoSolucao
-        self.contadorInteacoesSemMelhoria = contadorInteacoesSemMelhoria
+    def __init__(self,funçoesSolucao,valorEficiencia,contadorInteacoesSemMelhoria,eficiencia_fn):
+        self.funçoesSolucao = funçoesSolucao
         self.valorEficiencia = valorEficiencia
+        self.contadorInteacoesSemMelhoria = contadorInteacoesSemMelhoria
+        self.eficiencia_fn = eficiencia_fn
     def explore(self, index: int):
         tamanhoProblema = self.opcaoSolucao.shape[1]
 
         dimensao = random.randint(0,tamanhoProblema - 1)
 
         mutante = np.copy(self.opcaoSolucao)
+        mutante[dimensao] = mutante[dimensao] + (random.random() - 0.5) * 2
+
+        eficiencia_Do_Mutante = self.eficiencia_fn(mutante)
+
+        if eficiencia_Do_Mutante < self.valorEficiencia:
+            self.funçoesSolucao[index]  = mutante
+            self.valorEficiencia =[index] = mutante[dimensao]
+            self.contadorInteacoesSemMelhoria[index] = 0
+        else:
+            self.contadorInteacoesSemMelhoria += 1
